435ac06e7f27c3fdfd62f50247a9d4136560c26f
dmensah1 <dmensah4@uwo.ca>
2020-11-20 23:44:55 -0500

    Added explore page stuff

--- Front-End/FitFromHome/src/app/explore/explore.component.ts
+++ Front-End/FitFromHome/src/app/explore/explore.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explore.component.scss']
 })
 export class ExploreComponent implements OnInit {
+  classes = [
+    {
+      title: "Yoga with Demir!",
+      createdBy: "Demir Mensah",
+      description: "Beginner's yoga session. All you need is a yoga mat and some space around you!",
+      category: "Yoga",
+      price: "5",
+      startDate: new Date().getTime(),
+      endDate: (new Date().getTime() + 3600)
+    }
+  ];
+  coaches = [
+
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    // fetch & store classes + coach profiles
   }
 
 }
